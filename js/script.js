b36bdf1bb44874b6f7747389611feb9e62469734
Vincent Huang <vincentckhuang@gmail.com>
2016-06-28 11:19:09 -0700

    added event listener that registers clicks and applies turn correctly to squares

--- js/script.js
+++ js/script.js
@@ -1,11 +1,44 @@
 //models
-var board = ['x','','o','','','','','',''];
+var board = ['','','','','','','','',''];
 var turn = 'x';
 
+//selectors
+var htmlBoard = document.getElementsByClassName('square');
+
+//set up click listeners
+for (var i=0; i<board.length; i++) {
+	htmlBoard[i].addEventListener('click', function(e) {
+		console.log(e.target);
+		//loop through squares to check which node was clicked
+		for (var j=0; j<board.length; j++){
+			if (htmlBoard[j] === e.target) {
+				//check if board index already has something; 
+				if (board[j] == '') {
+					board[j] = turn;
+					//update board
+					drawBoard();
+					//switch turn
+					switchTurn();
+				}
+			}
+		}
+	});
+}
+
+//drawing model to screen
 function drawBoard() {
-	var htmlBoard = document.getElementsByClassName('square');
 	for (var i=0; i<board.length; i++) {
 		htmlBoard[i].innerHTML = board[i];
+	}
+}
+
+//switch turn
+function switchTurn() {
+	if (turn == 'x'){
+		turn = 'o';
+	}
+	else {
+		turn = 'x';
 	}
 }
 
