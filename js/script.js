3f3c9d10a1a63e6b1c52632956888ed5841b7e76
Vincent Huang <vincentckhuang@gmail.com>
2016-06-28 11:01:52 -0700

    fixed draw ting to board format

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 //models
-var board = ['','','','','','','','',''];
+var board = ['x','','o','','','','','',''];
 var turn = 'x';
 
+function drawBoard() {
+	var htmlBoard = document.getElementsByClassName('square');
+	for (var i=0; i<board.length; i++) {
+		htmlBoard[i].innerHTML = board[i];
+	}
+}
+
+drawBoard();
